b2fd2e16572251a08d2fa865a59c5674d01272b8
Mario Monteiro <marioammonteiro@gmail.com>
2021-07-18 00:42:07 +0100

    ...

--- packages/backend-nestjs/src/common/app/constants.ts
+++ packages/backend-nestjs/src/common/app/constants.ts
@@ -1,15 +1,7 @@
-import { CurrentUserPayload } from '../../auth/interfaces'
-import { UserRoles } from '../../auth/enums'
-
-// stub
 const GRAPHQL_PLAYGROUND_PATH: string = '/graphql';
 const API_RESPONSE_INTERNAL_SERVER_ERROR: string = 'Internal server error';
-// TODO
-// const CURRENT_USER_ADMIN_ROLE: CurrentUserPayload = { userId: null, username: null, roles: [UserRoles.ROLE_ADMIN, UserRoles.ROLE_USER] };
 
 export const appConstants = {
   GRAPHQL_PLAYGROUND_PATH,
   API_RESPONSE_INTERNAL_SERVER_ERROR,
-  // TODO
-  // CURRENT_USER_ADMIN_ROLE,
 };
